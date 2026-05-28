e39a925bbfea61c22f3e623672a63a7415e6be15
Sasha Petrenko <sap625@umsystem.edu>
2026-05-28 09:28:02 -0500

    Fix custom name

--- pysc2/maps/custom.py
+++ pysc2/maps/custom.py
@@ -1,6 +1,6 @@
 from pysc2.maps import lib
 
-class MyCustomMap(lib.Map):
+class TigerClaw(lib.Map):
     directory = "Custom"  # The subfolder within the Maps directory
     filename = "Tigerclaw"   # The filename without the .SC2Map extension
     players = 2              # Number of players (usually 1 for mini-games)
